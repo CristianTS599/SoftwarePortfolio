59410da1bde434aa103a61a02d5af4b1b13d2440
Cristian Torres Salazar <tor8@pdx.edu>
2026-04-08 22:30:11 -0700

    Hero Section updated for mobile support.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import Navbar from "./components/Internal/Navbar.tsx"
 import Hero from "./components/Internal/Hero.tsx"
 import Projects from "./components/Internal/Projects.tsx"
 import { Experience } from "./components/Internal/Experience.tsx"
 import EmailForm from "./components/Internal/EmailForm.tsx"
 import { useParams } from "react-router-dom"
-import Navbar2 from "./components/Internal/Navbar2.tsx"
+import Navbar from "./components/Internal/Navbar.tsx"
 
 export function App() {
   let params = useParams()
@@ -14,7 +13,7 @@ export function App() {
 
   return (
     <div className="mb-30">
-      <Navbar2 />
+      <Navbar />
       <Hero />
       <Projects />
       <Experience />
